64330cecb9e7925a3c478b921741812322e00411
REIGaruki <gyakujo@mail.ru>
2024-12-24 18:11:16 +0300

    Initial C

---
+++ src/main/java/pro/sky/grade/GradeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.grade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeApplication.class, args);
+	}
+
+}
